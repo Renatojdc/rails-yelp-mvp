50cfac64e6b537375f149a8ef97aef3bb402cb33
Renato Jardim Di Carlantonio <renatojdc@outlook.com>
2022-08-17 12:08:57 -0300

    recreated models and migrations to correct references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_17_142723) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_17_150158) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -22,12 +22,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_17_142723) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
-    t.text "content"
-    t.integer "restaurant_id_id", null: false
+    t.string "content"
+    t.integer "restaurant_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["restaurant_id_id"], name: "index_reviews_on_restaurant_id_id"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
-  add_foreign_key "reviews", "restaurant_ids"
+  add_foreign_key "reviews", "restaurants"
 end
