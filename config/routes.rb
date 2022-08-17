d463f0d69fa22d928bc02d2b4c625c003281e698
Renato Jardim Di Carlantonio <renatojdc@outlook.com>
2022-08-17 12:48:26 -0300

    create the controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :restaurants do
-    resouces :review, only: %i[new create]
+    resources :review, only: %i[new create]
   end
 end
