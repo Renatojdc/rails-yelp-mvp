7b7636f75efbd9c295a18af6cf89111ebfc7aade
Renato Jardim Di Carlantonio <renatojdc@outlook.com>
2022-08-17 12:45:23 -0300

    seed done.. resources done..

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.create(name: 'ChinaOutOfTheBox', address: 'Tijuca', category: 'chinese')
+
+Restaurant.create(name: 'Pizza Italia', address: 'Humaitá', category: 'italian')
+
+Restaurant.create(name: 'Yaki Village', address: 'Flamengo', phone_number: '21 9 9865-4748', category: 'japanese')
+
+Restaurant.create(name: 'Belgian', address: 'Grajaú', category: 'belgian')
+
+Restaurant.create(name: 'La nuit', address: 'Joá', phone_number: '21 9 8806-9798', category: 'french')
